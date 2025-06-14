c5d2ec2fb6a4d6be728feae42b4d6fcd85e48af0
LestathRiveraD <cesarrivera0404@gmail.com>
2025-06-14 16:02:39 -0700

    exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -24,28 +24,33 @@ const App = () => {
   const handleNewName = event => setNewName(event.target.value)
   const handleNewPhone = event => setNewPhone(event.target.value)
   const handleFilter = (event) => setFilter(event.target.value.toLowerCase())
-  const addNewNote = (event) => {
+  const addNewNumber = (event) => {
     event.preventDefault()
-    const a = persons.find((person) => {
-      return person.name === newName
-    })
+    const a = persons.find(person => person.name === newName)
 
     if (a)
+    {
       alert(`${newName} is already in phonebook`)
-    else
-      setPersons(persons.concat({name: newName, number: newPhone}))
+      return
+    }
+
+    let newEntry = {name: newName, number: newPhone}
+    axios.post('http://localhost:3001/persons', newEntry).then(res => {
+      setPersons(persons.concat(newEntry))
+    })
+
   }
 
   // UI
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Libro telefónico</h2>
       <Filter handleFilter={handleFilter} />
 
-      <h2>add new</h2>
-      <Add addNewNote={addNewNote} newName={newName} handleNewName={handleNewName} newPhone={newPhone} handleNewPhone={handleNewPhone}/>
+      <h2>Añadir número</h2>
+      <Add addNewNumber={addNewNumber} newName={newName} handleNewName={handleNewName} newPhone={newPhone} handleNewPhone={handleNewPhone}/>
 
-      <h2>Numbers</h2>
+      <h2>Números</h2>
       <Numbers persons={persons} filter={filter} />
     </div>
   )
