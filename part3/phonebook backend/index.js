b3f003e44cbe064a7755de803afce64bb4663d5c
LestathRiveraD <cesarrivera0404@gmail.com>
2025-08-10 12:33:16 -0700

    3.8

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -4,7 +4,12 @@ var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('content', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 
 var data = [
     { 
