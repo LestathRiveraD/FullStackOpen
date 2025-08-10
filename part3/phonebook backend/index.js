daef28ebd5637c6b8c73f9d685a86351df60f026
LestathRiveraD <cesarrivera0404@gmail.com>
2025-08-10 11:24:05 -0700

    3.6

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-
 const app = express()
+
+app.use(express.json())
 
 var data = [
     { 
@@ -23,7 +24,7 @@ var data = [
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
-]
+  ]  
 
 app.get('/info', (req, res) => {
     let d = new Date()
@@ -49,6 +50,38 @@ app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id
     data = data.filter(entry => entry.id !== id)
 
+    res.status(204).end()
+})
+
+app.post('/api/persons/', (req, res) => {
+    const newEntry = req.body
+
+    if(JSON.stringify(Object.keys(newEntry)) !== JSON.stringify(['name', 'number']))
+    {
+      console.log("{ error: 'invalid request format' }")      
+      console.log(Object.keys(newEntry))      
+      res.status(400).end()
+      return
+    }
+
+    if(data.find(entry => newEntry.name === entry.name))
+    {
+      console.log("{ error: 'name must be unique' }")
+      res.status(400).end()
+      return
+    }
+
+    newEntry.id = Math.floor((Math.random() * 9999999999) + 1); 
+
+    if(data.find(entry => newEntry.id === entry.id))
+    {
+      console.log("{ error: 'Unexpected error. Please try again.' }")
+      res.status(422).end()
+      return
+    }
+
+    console.log("POST ", newEntry)
+    data.push(newEntry)
     res.status(204).end()
 })
 
